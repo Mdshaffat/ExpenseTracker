5ee301842d8878019d26d389f5bc2b853f328f77
Md Shaffat <shaffat1995@outlook.com>
2023-01-27 01:58:18 +0600

    Example : Repository Pattern Added For Expenses

---
+++ ExpenseTracker/ExpenseTracker.Data/Repository/ExpensesRepository.cs
@@ -0,0 +1,46 @@
+﻿using ExpenseTracker.Core.Models;
+using ExpenseTracker.DataAccess.Data;
+using ExpenseTracker.DataAccess.Paging;
+using ExpenseTracker.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ExpenseTracker.DataAccess.Repository
+{
+    public class ExpensesRepository : IExpensesRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public ExpensesRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<PaginatedList<Expenses>> ExpensesListAsync(DateTime? fromDate,
+                                                                     DateTime? toDate,
+                                                                     int? pageNumber)
+        {
+            int pageSize = 10;
+            IQueryable<Expenses> expenseTrackerContext = context.Expense.Include(e => e.ExpenseCategory);
+            if (fromDate != null && toDate != null)
+            {
+                expenseTrackerContext = expenseTrackerContext.Where(s => s.DateOfTheExpense.Date >= fromDate.Value.Date && s.DateOfTheExpense.Date <= toDate.Value.Date);
+            }
+            return await PaginatedList<Expenses>.CreateAsync(expenseTrackerContext.AsNoTracking(), pageNumber ?? 1, pageSize);
+        }
+        public async Task<Expenses> ExpenseByIdAsync(int id)
+        {
+            var expenses = await context.Expense
+                .Include(e => e.ExpenseCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return expenses;
+        }
+
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Data/Repository/IRepository/IExpensesRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseTracker.Core.Models;
+using ExpenseTracker.DataAccess.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ExpenseTracker.DataAccess.Repository.IRepository
+{
+    public interface IExpensesRepository
+    {
+        Task<PaginatedList<Expenses>> ExpensesListAsync(DateTime? fromDate,
+                                                        DateTime? toDate,
+                                                        int? pageNumber);
+        Task<Expenses> ExpenseByIdAsync(int id);
+    }
+}
